287247f118104aa50574e226c4a8dc736c8db258
Alex Brown <ab01172@surrey.ac.uk>
2017-11-05 13:47:14 +0000

    Change to HAML, SASS and hardcoded strings

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,5 +5,16 @@ class HomeControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    
+    assert_template layout: 'application'
+    
+    assert_select 'title', 'CSZone'
+    assert_select 'h1', 'Home of Computer Science content.'
+    
+  end
+    
 end
